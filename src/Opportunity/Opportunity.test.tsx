cee9351a8ddbe4f27631949174dddaf9c0f76b81
Emmalu Fox <emmalu.fox@softwire.com>
2020-03-02 16:54:20 +0000

    Removed package.lock.json Updated

--- src/Opportunity/Opportunity.test.tsx
+++ src/Opportunity/Opportunity.test.tsx
@@ -1,3 +1,3 @@
 ﻿import React from 'react';
 import { render } from '@testing-library/react';
-import './Opportunity';
+import './Opportunity.tsx';
--- src/Spirit/Spirit.test.tsx
+++ src/Spirit/Spirit.test.tsx
@@ -1,3 +1,3 @@
 ﻿import React from 'react';
 import { render } from '@testing-library/react';
-import 'Spirit';
+import 'Spirit.tsx';
