0ae9755d8c798ff855fe1632e135f1114bf0decb
Jamie Baird <james.baird@softwire.com>
2020-03-02 15:46:24 +0000

    Added css classes and links

---
+++ src/NavBar/NavBar.tsx
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+import {Link} from 'react-router-dom';
+import logo from "./logo.svg";
+
+function NavBar(){
+
+    return (
+
+        <div className="navBar bar">
+
+            <Link className="navBar logo" to='/home'>Logo</Link>
+            <ul className="navBar menu">
+                <li className="navBar navLink">
+                    <Link to='/curiosity'>Curiosity</Link>
+                </li>
+                <li className="navBar navLink">
+                    <Link to='/opportunity'>Opportunity</Link>
+                </li>
+                <li className="navBar navLink">
+                    <Link to='/spirit'>Spirit</Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export {NavBar};
