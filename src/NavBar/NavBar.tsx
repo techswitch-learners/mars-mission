48af9584ead4d90b4a435086b07dd7bb685e6884
Jamie Baird <james.baird@softwire.com>
2020-03-03 14:34:06 +0000

    Hamburger menu placed on right hand side.
    Need to create media query for ipad and larger to remove hamburger

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -5,25 +5,27 @@ import './NavBar.scss';
 function NavBar(){
 
     return (
-
-        <nav className="navBar">
-            <input className="menuButton" type="checkbox" id="menuButton"/>
-            <label className="menuIcon" htmlFor="menuButton">
-                <span className="navIcon"></span>
-            </label>
-            <Link to='/home'><img className="logo" src="nasa-logo.png"/></Link>
-            <ul className="menu">
-                <li className="navItem">
-                    <Link className="navLink" to='/curiosity'>Curiosity</Link>
-                </li>
-                <li className="navItem">
-                    <Link className="navLink" to='/opportunity'>Opportunity</Link>
-                </li>
-                <li className="navItem">
-                    <Link className="navLink" to='/spirit'>Spirit</Link>
-                </li>
-            </ul>
-        </nav>
+<div>
+    <nav className="navBar">
+        <input className="menuButton" type="checkbox" id="menuButton"/>
+        <label className="menuIcon" htmlFor="menuButton">
+            <span className="navIcon"></span>
+        </label>
+        <Link to='/home'><img className="logo" src="nasa-logo.png"/></Link>
+        <ul className="menu">
+            <li className="navItem">
+                <Link className="navLink" to='/curiosity'>Curiosity</Link>
+            </li>
+            <li className="navItem">
+                <Link className="navLink" to='/opportunity'>Opportunity</Link>
+            </li>
+            <li className="navItem">
+                <Link className="navLink" to='/spirit'>Spirit</Link>
+            </li>
+        </ul>
+    </nav>
+    
+</div>
         
     );
 }
