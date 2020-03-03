4b551602ad94b4888fcbe17e39eac2419ed32a18
Jamie Baird <james.baird@softwire.com>
2020-03-03 11:56:26 +0000

    Edited test to check if nav link is rendered

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import {Curiosity} from "./Curiosity";
 
 test('check app renders and page contains header text', () => {
     let app = render(<App />);
-    const header = app.getByText('Mars Mission');
+    const header = app.getByText('Curiosity');
     expect(header).toBeInTheDocument();
 });
+
+
 
