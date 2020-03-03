077e45db1f80499b03ee752cb8f7c8d974522659
Emmalu Fox <emmalu.fox@softwire.com>
2020-03-03 13:45:58 +0000

    -Added APOD Api fetcher & test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import {parseApiData} from "./Components/ApiFetch/ApodApiFetch";
 
 
-test('check app renders and page contains header text', () => {
-    let app = render(<App />);
-    const header = app.getByText('Mars Mission');
-    expect(header).toBeInTheDocument();
+test('check that function returns JSON', () => {
+    expect(parseApiData()).not.toBeNull();
 });
 
