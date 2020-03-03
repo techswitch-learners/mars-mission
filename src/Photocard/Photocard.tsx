4f73bb838ad18c5a85cf8440f82d4f1f43ac261d
Jamie Baird <james.baird@softwire.com>
2020-03-03 11:09:07 +0000

    Created Photocard and related Url type

---
+++ src/Photocard/Photocard.tsx
@@ -0,0 +1,21 @@
+﻿﻿import React from "react";
+import {Url} from "../Models/Url"
+import "./Photocard.scss";
+
+interface PhotocardProps {
+    imageUrl: Url;
+    altText : string;
+    description : string;
+}
+
+function Photocard (props : PhotocardProps) {
+    return (
+        <div className="photocard">
+            <img className="photocardImage" 
+                 src={props.imageUrl} 
+                 alt={props.altText}/>
+            <p>desc</p>
+        </div>
+    )
+}
+export {Photocard}
