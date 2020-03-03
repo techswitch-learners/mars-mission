9f1f99802378776c8afbf7fe4af8d984d1c3fdd6
espe-on <eljoe0@googlemail.com>
2020-03-03 14:19:21 +0000

    Created basic structure for Rover Nav Menu

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,19 @@
+﻿﻿import React from "react";
+import "./Home.scss";
+import {RoverNavItem} from "./RoverNavItem";
+
+export function Home() {
+    return (
+        <ul className="roverNav">
+            <RoverNavItem imageUrl={"https://robohash.org/CuriosityRover"} 
+                          altText={"Curiosity Rover"} 
+                          description={"Curiosity"}/>
+            <RoverNavItem imageUrl={"https://robohash.org/OpportunityRover"} 
+                          altText={"Opportunity Rover"} 
+                          description={"Opportunity"}/>
+            <RoverNavItem imageUrl={"https://robohash.org/SpiritRover"} 
+                          altText={"Spirit Rover"} 
+                          description={"Spirit"}/>
+        </ul>
+    )
+}
---
+++ src/Pages/Home/RoverNavItem.tsx
@@ -0,0 +1,18 @@
+﻿import React from "react";
+import {Url} from "../../Models/Url"
+
+
+interface roverNavItemProps {
+    imageUrl: Url;
+    altText: string;
+    description: string;
+}
+
+export function RoverNavItem (props : roverNavItemProps){
+    return (
+        <li className="roverNavItem">
+            <img className="roverNavImage" src={props.imageUrl} alt={props.altText} />
+            <p className="roverNavLink">{props.description}</p>
+        </li>
+    )
+}
