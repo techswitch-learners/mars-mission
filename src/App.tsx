e7d1bad1015565c001b362c931129452b042232c
Wendy <alteredco@gmail.com>
2020-03-03 21:13:04 +0000

    Delete test route for date input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
 import React from 'react';
 import './App.scss';
-import {Footer} from "./Footer/Footer";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {Curiosity} from "./Pages/CuriosityLandingPage/CuriosityLandingPage";
 import {Spirit} from "./Pages/SpiritLandingPage/SpiritLandingPage";
 import {Opportunity} from "./Pages/OpportunityLandingPage/OpportunityLandingPage";
 import {NavBar} from "./NavBar/NavBar";
-import {Home} from "./Pages/Home/Home";
-import {DiscoverRoversPage} from "./Pages/DiscoverRoversPage/DiscoverRoversPage";
+import {DateInputForm} from "./Components/DateInputForm/DateInputForm";
+
 
 function App() {
-    
   return (
       <Router>
             <div>
-              <header className="header" data-testid="header">
+              <header className="header">
                   <NavBar />
               </header>
                 <Switch>
-                    <Route exact path="">
-                        <Home />
-                    </Route>
                     <Route exact path="/curiosity">
                         <Curiosity />
-                    </Route>
-                    <Route exact path="/curiosity/discover-rovers">
-                        <DiscoverRoversPage />
                     </Route>
                     <Route path="/spirit">
                         <Spirit />
@@ -34,10 +26,9 @@ function App() {
                         <Opportunity />
                     </Route>
                 </Switch>
-                <Footer/>
             </div>
       </Router>
   );
-} 
+}
 
 export default App;
