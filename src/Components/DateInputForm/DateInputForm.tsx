ded89a3ee9c8c528cae912fa931446d847674739
Wendy <alteredco@gmail.com>
2020-03-04 13:21:59 +0000

    Date form max will now always be today's date

--- src/Components/DateInputForm/DateInputForm.tsx
+++ src/Components/DateInputForm/DateInputForm.tsx
@@ -3,13 +3,18 @@ import "./DateInputForm.scss"
 
 
 export function DateInputForm() {
+    function getDateToday(){
+        let today: string = new Date().toISOString().split("T")[0];
+        return today;
+    }
+    
     return (
         <div >
             <form className="form">
                 <label>DATE: 
                     <br/>
                     <input type="date" id="date" name="date"
-                           min="12-08-05" max="2020-03-03" />
+                           min="12-08-05" max={getDateToday()}/>
                 </label>
                 <input type="submit" name="Submit" />
             </form>
