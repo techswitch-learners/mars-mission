87a5c6e576936a4f186e77d276e0405eef6a6996
Kanwaljit <cavey.waveyy@gmail.com>
2020-03-04 21:42:34 +0000

    new page for humans at nasa

---
+++ src/Pages/HumansAtNasa/HumansAtNasa.tsx
@@ -0,0 +1,11 @@
+﻿import {NavBar} from "../../NavBar/NavBar";
+﻿import React from "react";
+
+export function HumansAtNasa() {
+    return (
+        <div>
+            <NavBar/>
+            <h2>Humans At Nasa</h2>
+        </div>
+    );
+} 
