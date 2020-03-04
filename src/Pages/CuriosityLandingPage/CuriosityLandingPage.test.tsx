3463826df6236d2d1df6fa5ad5580e31e8a23317
Jamie Baird <james.baird@softwire.com>
2020-03-04 15:50:02 +0000

    moved test page to Curiosity Landing Page Directory

--- src/Pages/CuriosityLandingPage/CuriosityLandingPage.test.tsx
+++ src/Pages/CuriosityLandingPage/CuriosityLandingPage.test.tsx
@@ -1,5 +1,5 @@
-﻿import {render} from "@testing-library/react";
-import {Curiosity} from "../../Pages/CuriosityLandingPage/CuriosityLandingPage";
+﻿﻿import {render} from "@testing-library/react";
+import {Curiosity} from "./CuriosityLandingPage";
 import React from "react";
 
 test('check Hero component renders on page', () => {
