f45798af7f1a121769d98e48aed5198eb288360d
Emmalu Fox <emmalu.fox@softwire.com>
2020-03-04 15:16:17 +0000

    Full test of media switcher in HeroImageOfTheDay added, refactor of HeroImageOfTheDay

--- src/Components/HeroImageOfTheDay/HeroImageOfTheDay.tsx
+++ src/Components/HeroImageOfTheDay/HeroImageOfTheDay.tsx
@@ -2,39 +2,47 @@
 import {getImageOfTheDay, ImageOfTheDay} from "../ApiFetch/ApodApiFetch";
 
 
-function HeroImageOfTheDay() {
-    const [imageOfTheDay, setImageOfTheDay] = useState<ImageOfTheDay>(
-        {url:"", title:"", date:"", explanation:"", media_type:""}
-    );
+export function HeroImageOfTheDay() {
+    const [imageOfTheDay, setImageOfTheDay] = useState<ImageOfTheDay | null>(null);
+    
     useEffect(() => {
-        getImageOfTheDay().then(image => setImageOfTheDay(image));
+        getImageOfTheDay()
+            .then(image => setImageOfTheDay(image));
     }, []);
-    const photoViewer = <img className="contentImage"
-                             src={imageOfTheDay.url}
-                             alt={imageOfTheDay.date}/>;
-    const videoViewer = <iframe className="contentImage"
-                             src={imageOfTheDay.url}
-                             />;
-
-    function SwitchViewerType(media_type: string) {
-        if (media_type === "video") {
-            return videoViewer;
-
-        } else {
-            return photoViewer;
-        }
-        
+    
+    if (!imageOfTheDay) {
+        return <div>Loading</div>
     }
+    
     return (
         <div className="imageOfTheDay">
-            {SwitchViewerType(imageOfTheDay.media_type)}
+            <MediaViewer  imageOfTheDay={imageOfTheDay}/>
             <h3 className="contentTitle">{imageOfTheDay.title}</h3>
             <p className="contentDescription">{imageOfTheDay.explanation}</p>
         </div>
     )
 }
 
-export {HeroImageOfTheDay}
+interface MediaViewerProps {
+    imageOfTheDay: ImageOfTheDay
+}
+
+function MediaViewer(props: MediaViewerProps): JSX.Element {
+    if (props.imageOfTheDay.media_type == "video") {
+        return (
+            <iframe data-testid={"video-player"} className="contentImage"
+                    src={props.imageOfTheDay.url}
+            />
+        );
+    }
+    
+    return (
+        <img className="contentImage"
+             data-testid={"image-viewer"}
+             src={props.imageOfTheDay.url}
+             alt={props.imageOfTheDay.title}/>
+    );
+}
 
 
 
