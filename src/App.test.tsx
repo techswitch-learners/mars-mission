bd0b8f045bb5701700467ed80a86c6602916d54c
Jamie Baird <james.baird@softwire.com>
2020-03-04 14:27:28 +0000

    created test for Discover Rovers Page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import {Curiosity} from "./Pages/CuriosityLandingPage/CuriosityLandingPage";
 
 test('check app renders and page contains header text', () => {
     let app = render(<App />);
--- src/Pages/DiscoverRoversPage/DiscoverRoversPage.tsx
+++ src/Pages/DiscoverRoversPage/DiscoverRoversPage.tsx
@@ -2,14 +2,14 @@
 import {Photocard} from "../../Photocard/Photocard";
 import "./DiscoverRoversPage.scss"
 
-export function DiscoverRoversPage(){
+export function DiscoverRoversPage(): JSX.Element{
     return (
         <div className="discoverRovers">
             <h1>Discover Rovers</h1>
             <h3>Meet the Rovers on The Red Planet</h3>
-            <Photocard imageUrl={"https://spaceplace.nasa.gov/mars-sojourner/en/sojourner-first.en.jpg"} altText={"Sojourner Rover"} imageTitle={"Sojourner Rover"} description={"Sojourner is a rover that landed on Mars in 1997 in a location called Ares Vallis where it explored and took many photos."}/>
-            <Photocard imageUrl={"https://spaceplace.nasa.gov/mars-spirit-opportunity/en/spirit-opportunity.en.jpg"} altText={"Spirit and Opportunity Rovers"} imageTitle={"Spirit and Opportunity Rovers"} description={"Spirit and Opportunity are twin rovers that were made to learn more about the planet Mars."}/>
-            <Photocard imageUrl={"https://spaceplace.nasa.gov/mars-curiosity/en/curiosity-here.en.jpg"} altText={"Curiosity Rover"} imageTitle={"Curiosity Rover"} description={"Curiosity is a rover that was sent to Mars to determine if the Red Planet ever had the proper conditions for microbial life to survive."}/>
+            <Photocard imageUrl={"https://spaceplace.nasa.gov/mars-sojourner/en/sojourner-first.en.jpg"} altText={"Sojourner Rover"} imageTitle={"Sojourner Rover"} description={"Sojourner is a rover that landed on Mars in 1997 in a location called Ares Vallis where it explored and took many photos."} testid={"sojournerPhotocard"}/>
+            <Photocard imageUrl={"https://spaceplace.nasa.gov/mars-spirit-opportunity/en/spirit-opportunity.en.jpg"} altText={"Spirit and Opportunity Rovers"} imageTitle={"Spirit and Opportunity Rovers"} description={"Spirit and Opportunity are twin rovers that were made to learn more about the planet Mars."} testid={"spiritAndOpportunityPhotocard"}/>
+            <Photocard imageUrl={"https://spaceplace.nasa.gov/mars-curiosity/en/curiosity-here.en.jpg"} altText={"Curiosity Rover"} imageTitle={"Curiosity Rover"} description={"Curiosity is a rover that was sent to Mars to determine if the Red Planet ever had the proper conditions for microbial life to survive."} testid={"curiosityPhotocard"}/>
         </div>
     )
 }
