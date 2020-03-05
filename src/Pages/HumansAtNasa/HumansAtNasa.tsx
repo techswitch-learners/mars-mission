ca9986c66be6e700695ebc7cfb8a31f2a918099e
Emmalu Fox <emmalu.fox@softwire.com>
2020-03-05 12:10:22 +0000

    Api working

--- src/Pages/HumansAtNasa/HumansAtNasa.tsx
+++ src/Pages/HumansAtNasa/HumansAtNasa.tsx
@@ -2,15 +2,17 @@
 import {NavBar} from "../../NavBar/NavBar";
 import {HumanAtNasa} from "../../Components/HumanAtNasa/HumanAtNasa";
 import './HumansAtNasa.scss';
+import {SetHumansInSpace} from "./HumansInSpaceApiFetch";
 
 export function HumansAtNasa() {
     return (
         <div className='humansAtNasaBody'>
-            <h1>Humans At Nasa</h1>
+            <h1 className='opportunity'>Humans At Nasa</h1>
             <p>NASA's Mars missions are designed, built and operated by some of the best and brightest. Meet some of the amazing people who do their best work on another planet.</p>
             <HumanAtNasa name={"Laura Kerber"} jobTitle={"Research Scientist"} jobDescription={"Laura Kerber is a research scientist at JPL with interests in physical volcanology, aeolian geomorphology, wind over complex surfaces, the ancient Martian climate."} imageUrl={"https://science.jpl.nasa.gov/images/people-3979.jpg"}/>
             <HumanAtNasa name={"Dr. Timothy Parker"} jobTitle={"Geology Team Group Lead"} jobDescription={"Dr. Tim Parker was the originator of the Mars Ocean hypothesis. He earned his Ph.D. at the University of Southern California in 1994, with a dissertation entitled \"Martian Paleolakes and Oceans.\""} imageUrl={"https://science.jpl.nasa.gov/images/people-135.jpg"}/>
             <HumanAtNasa name={"Nathan Williams"} jobTitle={"Planetary Geologist"} jobDescription={"Nathan Williams is a planetary geologist with particular interest in the remote sensing of planetary surfaces, especially relating to structural geology, geodesy, and landform evolution."} imageUrl={"https://science.jpl.nasa.gov/images/people-4770.jpg"}/>
+            <SetHumansInSpace/>
         </div>
 
     );
---
+++ src/Pages/HumansAtNasa/HumansInSpaceApiFetch.tsx
@@ -0,0 +1,33 @@
+﻿import {useEffect, useState} from "react";
+import React from "react";
+
+export interface HumansInSpace {
+    people: {craft: string; name: string}[];
+    number: string;
+}
+
+const apiUrl: string = `http://api.open-notify.org/astros.json`;
+
+export async function getHumansInSpace(): Promise<HumansInSpace>  {
+    const apiResponse = await fetch(apiUrl);
+    return await apiResponse.json();
+}
+
+export function SetHumansInSpace() {
+    const [humansInSpace, setHumansInSpace] = useState<HumansInSpace | null>(null);
+
+    useEffect(() => {
+        getHumansInSpace()
+            .then(astronauts => setHumansInSpace(astronauts));
+    }, []);
+    if (!humansInSpace) {
+        return (<div>Loading</div>)
+    }
+    const humansInSpaceList = humansInSpace.people;
+    let astronautList: any = [];
+    
+    humansInSpaceList.map(human=> astronautList.push(human.name, human.craft));
+    return (
+        <div>{astronautList}</div>
+    )
+}
