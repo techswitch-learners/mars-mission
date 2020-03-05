e4f2f0dc2c1261bdaf59fb95a1bdbc95fe780554
Kanwaljit <cavey.waveyy@gmail.com>
2020-03-05 20:00:07 +0000

    changes to humans at nasa

--- src/Pages/HumansAtNasa/HumansAtNasa.tsx
+++ src/Pages/HumansAtNasa/HumansAtNasa.tsx
@@ -11,6 +11,5 @@ export function HumansAtNasa() {
             <HumanAtNasa name={"Dr. Timothy Parker"} jobTitle={"Geology Team Group Lead"} jobDescription={"Dr. Tim Parker was the originator of the Mars Ocean hypothesis. He earned his Ph.D. at the University of Southern California in 1994, with a dissertation entitled \"Martian Paleolakes and Oceans.\""} imageUrl={"https://science.jpl.nasa.gov/images/people-135.jpg"}/>
             <HumanAtNasa name={"Nathan Williams"} jobTitle={"Planetary Geologist"} jobDescription={"Nathan Williams is a planetary geologist with particular interest in the remote sensing of planetary surfaces, especially relating to structural geology, geodesy, and landform evolution."} imageUrl={"https://science.jpl.nasa.gov/images/people-4770.jpg"}/>
         </section>
-            
     );
 } 
