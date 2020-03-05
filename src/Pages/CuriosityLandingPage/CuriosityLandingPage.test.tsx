b9be833f376ecb40e4079f7b9991af29309b7223
Beatrice <beatrice.opara@softwire.com>
2020-03-05 15:08:38 +0000

    fixed routing bug on test page

--- src/Pages/CuriosityLandingPage/CuriosityLandingPage.test.tsx
+++ src/Pages/CuriosityLandingPage/CuriosityLandingPage.test.tsx
@@ -1,9 +1,14 @@
 ﻿﻿import {render} from "@testing-library/react";
 import {Curiosity} from "./CuriosityLandingPage";
 import React from "react";
+import {BrowserRouter as Router} from "react-router-dom";
 
 test('check Hero component renders on page', () => {
-    let curiosityPage = render(<Curiosity />);
+    let curiosityPage = render(
+        <Router>
+            <Curiosity />    
+        </Router>
+        );
     const hero = curiosityPage.getByTestId("hero");
     expect(hero).toBeInTheDocument();
 });
