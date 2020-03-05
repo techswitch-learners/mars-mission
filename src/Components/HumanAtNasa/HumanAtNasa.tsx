0676bef41468c3a4776bec5fdde90f85980629ce
Emmalu Fox <emmalu.fox@softwire.com>
2020-03-05 11:17:28 +0000

    Initial Commit

---
+++ src/Components/HumanAtNasa/HumanAtNasa.tsx
@@ -0,0 +1,22 @@
+﻿﻿import React from 'react';
+import './HumanAtNasa.scss';
+
+interface HumanAtNasaProps{
+    name : string;
+    jobTitle : string;
+    jobDescription: string;
+    imageUrl : string;
+}
+
+export function HumanAtNasa(props : HumanAtNasaProps){
+    
+    return(
+        <div className='humansComponent'>
+            <img className='humansImage' src={props.imageUrl}/>
+            <h2 className='humanNames'>{props.name}</h2>
+            <h3 className='jobTitle'>{props.jobTitle}</h3>
+            <p className='jobDescription'>{props.jobDescription}</p>
+        </div>
+    );
+    
+}
