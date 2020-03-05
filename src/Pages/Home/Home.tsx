88934fada4344daad4ee36bad768393258813081
hnalabanda <hemanalabanda@gmail.com>
2020-03-05 15:36:18 +0000

    changed media query and scss file

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -9,17 +9,17 @@ export function Home(): JSX.Element {
         <div>
         <HeroImageOfTheDay/>
         <ul className="roverNav">
-            <RoverNavItem imageUrl={"https://image.freepik.com/free-vector/group-people-holding-question-mark-icons_53876-64627.jpg"}
+            <RoverNavItem imageUrl={`${process.env.PUBLIC_URL}/Images/curiosity.jpg`}
                           align={"rightAlign"}
                           altText={"Curiosity Rover"}
                           description={"Curiosity"}
                           destination={"/curiosity"}/>
-            <RoverNavItem imageUrl={"https://media.npr.org/assets/img/2018/06/15/mom-support_slide-b7832f16b707e5bde84de23c0277f0328d7bd6f9-s800-c85.jpg"}
+            <RoverNavItem imageUrl={`${process.env.PUBLIC_URL}/Images/opportunity.jpg`}
                           align={"leftAlign"}
                           altText={"Opportunity Rover"}
                           description={"Opportunity"}
                           destination={"/opportunity"}/>
-            <RoverNavItem imageUrl={"https://image.freepik.com/free-vector/teenagers-friends-having-fun-cartoons_18591-52609.jpg"}
+            <RoverNavItem imageUrl={`${process.env.PUBLIC_URL}/Images/spirit.jpg`}
                           align={"rightAlign"}
                           altText={"Spirit Rover"}
                           description={"Spirit"}
