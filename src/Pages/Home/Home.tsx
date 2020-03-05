e8af94352ea0300318f0ac4cbacbdd5b5083ef6b
hnalabanda <hemanalabanda@gmail.com>
2020-03-05 12:56:09 +0000

    changes to the landing page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -9,17 +9,17 @@ export function Home(): JSX.Element {
         <div>
         <HeroImageOfTheDay/>
         <ul className="roverNav">
-            <RoverNavItem imageUrl={"https://robohash.org/CuriosityRover?bgset=bg1"}
+            <RoverNavItem imageUrl={"https://image.freepik.com/free-vector/group-people-holding-question-mark-icons_53876-64627.jpg"}
                           align={"rightAlign"}
                           altText={"Curiosity Rover"}
                           description={"Curiosity"}
                           destination={"/curiosity"}/>
-            <RoverNavItem imageUrl={"https://robohash.org/OpportunityRover?bgset=bg1"}
+            <RoverNavItem imageUrl={"https://media.npr.org/assets/img/2018/06/15/mom-support_slide-b7832f16b707e5bde84de23c0277f0328d7bd6f9-s800-c85.jpg"}
                           align={"leftAlign"}
                           altText={"Opportunity Rover"}
                           description={"Opportunity"}
                           destination={"/opportunity"}/>
-            <RoverNavItem imageUrl={"https://robohash.org/SpiritRover?bgset=bg1"}
+            <RoverNavItem imageUrl={"https://image.freepik.com/free-vector/teenagers-friends-having-fun-cartoons_18591-52609.jpg"}
                           align={"rightAlign"}
                           altText={"Spirit Rover"}
                           description={"Spirit"}
