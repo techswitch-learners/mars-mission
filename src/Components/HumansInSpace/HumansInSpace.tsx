fc47c621444cc56e189e0a5a8a2624fbafd92623
Emmalu Fox <emmalu.fox@softwire.com>
2020-03-05 13:26:03 +0000

    Added general use border style

--- src/Components/HumansInSpace/HumansInSpace.tsx
+++ src/Components/HumansInSpace/HumansInSpace.tsx
@@ -1,11 +1,13 @@
 ﻿import React from "react";
+import "../../ConstantsMixins.scss";
+import "./HumansInSpace.scss";
 import {astronautList, SetHumansInSpace} from "../../Pages/HumansAtNasa/HumansInSpaceApiFetch";
 
 export function HumansInSpace() {
     SetHumansInSpace();
     return (
         <div>
-            {astronautList}
+            <article className="astronautsInSpaceNow">{astronautList}</article>
         </div>
     )
 }
