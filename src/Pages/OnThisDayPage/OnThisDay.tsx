134591b10e9dfdac7cd83f44556a406370dbc495
Wendy <alteredco@gmail.com>
2020-03-06 11:19:00 +0000

    set up initial styles on OnThisDay

--- src/Pages/OnThisDayPage/OnThisDay.tsx
+++ src/Pages/OnThisDayPage/OnThisDay.tsx
@@ -2,13 +2,14 @@
 import {DateInputForm} from "../../Components/DateInputForm/DateInputForm";
 import {RoverGalleryImage} from "../../Components/RoverGalleryImage/RoverGalleryImage";
 import {Photocard} from "../../Components/Photocard/Photocard";
+import "./OnThisDay.scss";
 
 export function OnThisDayPage() {
     
     const [dateInput, setDateInput] = useState<string | null>(null);
     
     return (
-        <div>
+        <div className="onThisDay">
             {dateInput? <RoverGalleryImage dateInput={dateInput}/> : <Photocard imageUrl={"http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01004/opgs/edr/fcam/FLB_486615455EDR_F0481570FHAZ00323M_.JPG"} altText={"June 3, 2015 On Mars"} imageTitle={""} description={""} testid={"rover_default_image"}/>}
             <h3>Curiosity worked on Mars from August 6, 2012 until September 28, 2019.</h3>
             <h2>Please choose a date to see Mars on that day!</h2>
