d2fca3e80e086bd6b1ea081d0283166740cd3cc8
espe-on <eljoe0@googlemail.com>
2020-03-06 11:20:38 +0000

    reordering of router

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import {Footer} from "./Components/Footer/Footer";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {Home} from "./Pages/Home/Home";
 import {Curiosity} from "./Pages/CuriosityLandingPage/CuriosityLandingPage";
-import {Spirit} from "./Pages/SpiritLandingPage/SpiritLandingPage";
-import {Opportunity} from "./Pages/OpportunityLandingPage/OpportunityLandingPage";
-import {WeatherOnMarsPage} from "./Pages/WeatherOnMarsPage/WeatherOnMarsPage";
 import {DiscoverRoversPage} from "./Pages/DiscoverRoversPage/DiscoverRoversPage";
-import {InspirationalQuotesPage} from "./Pages/InspirationalQuotesPage/InspirationalQuotesPage";
 import {OnThisDayPage} from "./Pages/OnThisDayPage/OnThisDay";
+import {WeatherOnMarsPage} from "./Pages/WeatherOnMarsPage/WeatherOnMarsPage";
+import {Spirit} from "./Pages/SpiritLandingPage/SpiritLandingPage";
+import {InspirationalQuotesPage} from "./Pages/InspirationalQuotesPage/InspirationalQuotesPage";
+import {Opportunity} from "./Pages/OpportunityLandingPage/OpportunityLandingPage";
 import {HumansAtNasa} from "./Pages/HumansAtNasa/HumansAtNasa";
 
 
@@ -24,11 +24,11 @@ export default function App() {
                 <Route exact path="/curiosity" component={Curiosity}/>
                 <Route exact path="/curiosity/discover-rovers" component={DiscoverRoversPage}/>
                 <Route exact path="/curiosity/date-input" component={OnThisDayPage}/>
+                <Route exact path="/curiosity/weather-on-mars" component={WeatherOnMarsPage}/>
                 <Route exact path="/spirit" component={Spirit}/>
                 <Route exact path="/spirit/inspirational-quotes" component={InspirationalQuotesPage}/>
                 <Route exact path="/opportunity" component={Opportunity}/>
                 <Route exact path="/opportunity/humans" component={HumansAtNasa} />
-                <Route exact path="/curiosity/weather-on-mars" component={WeatherOnMarsPage}/>
             </Switch>
             <Footer/>
         </Router>
