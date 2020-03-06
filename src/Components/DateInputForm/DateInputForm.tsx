78760ad9a93bc2f77c5ebe193b2bbf718f461e80
espe-on <eljoe0@googlemail.com>
2020-03-06 11:21:20 +0000

    tidying

--- src/Components/DateInputForm/DateInputForm.tsx
+++ src/Components/DateInputForm/DateInputForm.tsx
@@ -1,32 +1,31 @@
 import React, {FormEvent, SetStateAction, useState} from "react";
 import "./DateInputForm.scss"
 
-
-
 interface DateInputFormProps {
-    dateInput : string | null;
-    setDateInput : (date: string) => void;
+    dateInput: string | null;
+    setDateInput: (date: string) => void;
 }
 
-export function DateInputForm(props : DateInputFormProps) {
+export function DateInputForm(props: DateInputFormProps) {
     const [date, setDate] = useState("");
-    
-    function getDateToday(){
+
+    function getDateToday() {
         let today: string = new Date().toISOString().split("T")[0];
         return today;
     }
-    
+
     function handleClick(event: FormEvent) {
         event.preventDefault();
         props.setDateInput(date);
     }
 
     return (
-        <div >
+        <div>
             <form data-testid={"on-this-day-form"} onSubmit={handleClick} className="form">
                 <label>DATE:
                     <br/>
-                    <input data-testid={"date-input"} value={date} onChange={event => setDate(event.target!.value)} type="date" id="date" name="date"
+                    <input data-testid={"date-input"} value={date} onChange={event => setDate(event.target!.value)}
+                           type="date" id="date" name="date"
                            min="12-08-05" max={getDateToday()}/>
                 </label>
                 <input type="submit" name="Submit"/>
