19d8443352691c06568dca96a74f7f45d481953f
espe-on <eljoe0@googlemail.com>
2020-03-06 11:16:49 +0000

    import of external type

--- src/Pages/Home/RoverNavItem.tsx
+++ src/Pages/Home/RoverNavItem.tsx
@@ -1,14 +1,16 @@
 ﻿import React from "react";
 import {Url} from "../../Models/Url"
+import {AlignType} from "../../Models/AlignType";
+import {RoverLinks} from "../../Models/RoverLinks";
 import {Link} from "react-router-dom";
 
 
 interface roverNavItemProps {
     imageUrl: Url;
-    align : 'leftAlign' | 'rightAlign';
+    align : AlignType;
     altText : string;
     description : string;
-    destination : '/curiosity' | '/opportunity' | '/spirit';
+    destination : RoverLinks;
 }
 
 export function RoverNavItem (props : roverNavItemProps):JSX.Element {
