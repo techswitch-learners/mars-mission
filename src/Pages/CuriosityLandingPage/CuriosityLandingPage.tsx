49bf1a31089d379137b21cd4ee4eea138f528402
Kanwaljit <cavey.waveyy@gmail.com>
2020-03-06 13:09:29 +0000

    deleted commented out messages

--- src/Pages/CuriosityLandingPage/CuriosityLandingPage.tsx
+++ src/Pages/CuriosityLandingPage/CuriosityLandingPage.tsx
@@ -28,29 +28,3 @@ export function Curiosity(): JSX.Element {
     );
 }
 
-/*
-export function Curiosity(): JSX.Element {
-    return (
-        <section className="hero-image">
-            <Hero 
-                headingText={"Curiosity"} 
-                rover={"curiosity"} 
-                imageUrl={"https://mars.nasa.gov/system/content_pages/main_images/20_overlay_2000x1000_curiosity.jpg"}/>
-            <ul className="childList">
-                <li>
-                    <Link className="childLink" to='/curiosity/on-this-day-on-mars'>On this day on Mars</Link>
-                </li>
-                <li>
-                    <Link className="childLink" to='/curiosity/discover-rovers'>Discover the Rovers</Link>
-                </li>
-                <li>
-                    <Link className="childLink" to='/curiosity/weather-on-mars'>Weather On Mars</Link>
-                </li>
-                <li>
-                    <Link className="childLink" to='/curiosity/life-on-mars'>Life On Mars</Link>
-                </li>
-            </ul>
-        </section>
-    );
-}
-*/
